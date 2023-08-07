35c00ac65aea4550ff48b7ddb257d2123d82f4c0
bfarnsworth <bfarnsworth@acm.org>
2023-08-06 20:59:44 -1000

    Filled out the remaining funcs; all appear to be working (not deeply tested)

--- internal/wee/url.go
+++ internal/wee/url.go
@@ -1,3 +1,4 @@
+// Module url provides functions to create, use, and retires wee Urls.
 package wee
 
 import (
@@ -7,43 +8,42 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-const (
-	testDest = "www.nytimes.com/games"
-)
-
-
-// TBD not sure if I can do this,
-// I want a this type so it can provide log & db as context
-// will it conflict with the gin Context?
-
 type Shortener struct {
-	repo  *Repository
+	repo   *Repository
 	logger *log.Logger
 }
 
 func NewShortener(r *Repository, l *log.Logger) *Shortener {
 	return &Shortener{
-		repo: r,
+		repo:   r,
 		logger: l}
 }
 
+// FollowUrl expands the weeUrl and redirects the client browser to the full URL.
 func (s *Shortener) FollowUrl(c *gin.Context) {
 	// this is where we redirect...
 	wee := c.Param("weeUrl")
+
+	// THIS SHOULD BE UNNECESSARY if the main routing r.GET pattern is correct
+	if wee == "" {
+		s.logger.Printf("found redirect route, ROUTER LOGIC IS WRONG")
+		c.File("./public/index.html")
+		return
+	}
 	rec, err := s.repo.find(wee)
 	if err != nil {
-		if testDest != "" {
-			c.Redirect(http.StatusTemporaryRedirect, testDest)
-			return
-		}
+		// this will be a common situation, anytime the weeUrl is mistyped (or just junk)
+		s.logger.Printf("could not find record for %s, %v\n", wee, err)
 		c.JSON(http.StatusNotFound, gin.H{
 			"reason": err.Error(),
 		})
 		return
 	}
+	s.logger.Printf("redirecting to %s\n", rec.Url)
 	c.Redirect(http.StatusTemporaryRedirect, rec.Url)
 }
 
+// ShortenUrl accepts a full URL string, creates record for it, and saves it.
 func (s *Shortener) ShortenUrl(c *gin.Context) {
 	url := c.PostForm("fullUrl")
 	rec := createRecord(url)
@@ -58,22 +58,45 @@ func (s *Shortener) ShortenUrl(c *gin.Context) {
 
 	c.Header("Content-Type", "application/json")
 	c.JSON(http.StatusOK, gin.H{
-		"weeUrl":   rec.Tag,
-		"token": rec.Token,
+		"weeUrl": rec.Tag,
+		"token":  rec.Token,
 	})
 }
 
+// LengthenUrl expands a weeUrl and returns it for display but does not redirect to it.
 func (s *Shortener) LengthenUrl(c *gin.Context) {
+	var rec *Record
 	wee := c.Param("weeUrl")
+
+	rec, err := s.repo.find(wee)
+	if err != nil {
+		s.logger.Printf("Error, could not find a record for wee URL %s: %v\n", wee, err)
+		c.JSON(http.StatusNotFound, gin.H{
+			"reason": err.Error(),
+		})
+		return
+	}
+
 	c.Header("Content-Type", "application/json")
 	c.JSON(http.StatusOK, gin.H{
-		"url": "https://https://www.nytimes.com/crosswords/game/mini/" + wee,
+		"url": rec.Url,
 	})
 }
 
+// RetireUrl accepts token, then locates the corresponding record and removes it.
 func (s *Shortener) RetireUrl(c *gin.Context) {
+	token := c.Param("token")
+
+	err := s.repo.remove(token)
+	if err != nil {
+		s.logger.Printf("Error, could not remove a record for token %s: %v\n", token, err)
+		c.JSON(http.StatusNotFound, gin.H{
+			"reason": err.Error(),
+		})
+		return
+	}
+
 	c.Header("Content-Type", "application/json")
 	c.JSON(http.StatusOK, gin.H{
-		"url": "gone",
 	})
 }
